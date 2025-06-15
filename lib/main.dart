c77d00b92e4c66659465018cdb5c0033b48a4cc2
Muhammad Dio Revansa <mhdiorevansa098@gmail.com>
2025-06-15 13:07:26 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:anime_streaming_app/ui/pages/welcome_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: WelcomePage());
+  }
+}
